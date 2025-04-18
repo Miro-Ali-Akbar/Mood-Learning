93c07f6108dff154d67cc5074b6a3ebd2ab5fb4f
Miro Ali Akbar <Miro-Ali-Akbar@github.com>
2025-04-18 21:09:47 +0200

    fixed patherror

--- code/preprocess.py
+++ code/preprocess.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 # Columns to remove
@@ -5,11 +6,12 @@ bad_columns = {
     "NOTE", "MENSTRUAL CYCLE", "WEIGHT", "MINDFULNESS", "THERAPY", "NEWS", "SOCIAL MEDIA", "CHORES", "ID", "EXERCISE"
 }
 
-frame = pd.read_csv("../data/entry.csv")
+frame = pd.read_csv("data/entry.csv")
 
 def discard_unwanted_columns(df, bad_columns):
     kept_cols = [col for col in df.columns if col not in bad_columns]
     cleaned_df = df[kept_cols]
+    cleaned_df = cleaned_df.replace(0, np.nan)
     # Be in root when running python
     cleaned_df.to_csv("data/data.csv", index=False)
 
